e394f5479ab2eb06c2fa2373a2f19f34b95786de
richard zhang <zjh991600@163.com>
2025-07-15 02:13:33 +0800

    修改启动配置

--- api/src/main/java/com/family/diary/api/Application.java
+++ api/src/main/java/com/family/diary/api/Application.java
@@ -1,13 +1,17 @@
 package com.family.diary.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@Slf4j
+@EnableAsync
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("昊昊和爸爸妈妈的小屋日记：后台启动成功！");
     }
-
 }
